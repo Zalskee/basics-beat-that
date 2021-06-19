e3715c4bdab0508717980e27aad85b58b4336fed
Zalskee <faizalnooraznan@gmail.com>
2021-06-19 15:39:45 +0800

    completed main function. to troubleshoot logic from line 80 onwards

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var Mode_ChooseOrder = 'Order Mode';
 
 // start with roll mode and P1 
 var gameMode = Mode_Roll;
-var currentPlayer = '1P';
+var currentPlayer = 1;
 
 // create arrays to store dice rolls
 var p1Dice = [];
@@ -24,7 +24,7 @@ var rollDice = function () {
 var get2Rolls = function () {
   var newRolls = [rollDice(), rollDice()];
 
-  if (currentPlayer == '1P') {
+  if (currentPlayer == 1) {
     p1Dice = newRolls;
   } else { 
     p2Dice = newRolls;
@@ -32,12 +32,12 @@ var get2Rolls = function () {
   return newRolls
 };
 
-// function to get player's number depending on order choice
-var getPlayerNumber = function(firstNumChoice) {
+// function to get player's number depending on combination choice
+var getPlayerNumber = function(combiChoice) {
   // create array for function
   var diceArray = [];
   // start by assuming 1P's turn and set array to p1Dice. Otherwise set to p2Dice
-  if (currentPlayer == '1P') {
+  if (currentPlayer == 1) {
     diceArray = p1Dice
   } else {
     diceArray = p2Dice
@@ -45,16 +45,15 @@ var getPlayerNumber = function(firstNumChoice) {
 
   // creat variable for player's number
   var playerNumber; 
-  // firstNumChoice is 1 (i.e. no change) 
-  if (firstNumChoice == 1) {
+  // combiChoice (1) is default 1-2 sequence. combiChoice (2) is 2-1 sequence.
+  if (combiChoice == 1) {
     playerNumber = Number(String(diceArray[0]) + String(diceArray[1]));
   } else {
     playerNumber = Number(String(diceArray[1]) + String(diceArray[0]));
   }
   console.log('player number: ' + playerNumber)
 
-
-  if (currentPlayer == '1P') {
+  if (currentPlayer == 1) {
     p1Num = playerNumber
   } else {
     p2Num = playerNumber
@@ -62,7 +61,6 @@ var getPlayerNumber = function(firstNumChoice) {
 
   return playerNumber
 };
-
 
 
 var main = function (input) {
@@ -73,12 +71,31 @@ var main = function (input) {
   if (gameMode == Mode_Roll) {
     var newRolls = get2Rolls();
     gameMode == Mode_ChooseOrder;
-    return `Welcome ${currentPlayer} <br> You rolled 1st Dice: ${newRolls[0]} and 2nd Dice : ${newRolls[0]} <br>
-  To move 2nd Dice as the first number, press 2. Otherwise, press 1 to keep current order.`;
-}
+    return `Welcome Player ${currentPlayer} <br> You rolled 1st Dice: ${newRolls[0]} and 2nd Dice : ${newRolls[0]} <br>
+  Please enter (1) for dice 1-2 combination or (2) for dice 2-1 combination.`;
+  }
 
-// To complete
+  if (gameMode == Mode_ChooseOrder) {
+    var combiChoice = Number(input);
+    var playerNumber = getPlayerNumber(combiChoice);
+    var printOrderMessage = `Player ${currentPlayer}, You chose combination ${combiChoice}. <br> Your number is ${playerNumber}.`;
+  }
 
+  // change to player 2
+
+  if (currentPlayer == 1) {
+    currentPlayer == 2;
+    gameMode = Mode_Roll;
+    return `${printOrderMessage} <br> It is now Player 2's turn. Press submit to roll Player 2's dice.`;
+  }
+
+  if (p1Num < p2Num) {
+    return `Player 2's number is ${p2Num}. Player 1's number is ${p1Num}. Player 2 won!`
+  }; 
+  if (p2Num < p1Num) {
+    return `Player 1's number is ${p1Num}. Player 2's number is ${p2Num}. Player 1 won!`
+  }; 
+  
 
 };
 
